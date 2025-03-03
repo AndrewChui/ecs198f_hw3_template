fe9c965197edf506099c9a765a50505e24e180ac
Andrew Chui <kcchui@ucdavis.edu>
2025-03-02 23:06:32 -0800

    changes to test cases

--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -1,27 +1,31 @@
 import pytest
-from foo_bar_baz import foo_bar_baz
 
 # Add testcases Here
 
 def test_easy_cases():
+    from foo_bar_baz import foo_bar_baz
     assert foo_bar_baz(1) == "1"
     assert foo_bar_baz(2) == "1 2"
 
 def test_multiple_of_three():
+    from foo_bar_baz import foo_bar_baz
     assert foo_bar_baz(3) == "1 2 Foo"
     assert foo_bar_baz(6) == "1 2 Foo 4 Bar Foo"
     assert foo_bar_baz(9) == "1 2 Foo 4 Bar Foo 7 8 Foo"
 
 def test_multiple_of_five():
+    from foo_bar_baz import foo_bar_baz
     assert foo_bar_baz(5) == "1 2 Foo 4 Bar"
     assert foo_bar_baz(10) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar"
     assert foo_bar_baz(15) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz"
 
 def test_multiple_of_three_and_five():
+    from foo_bar_baz import foo_bar_baz
     assert foo_bar_baz(15) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz"
     assert foo_bar_baz(30) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz 16 17 Foo 19 Bar Foo 22 23 Foo Bar 26 Foo 28 29 Baz"
 
 def test_edge_cases():
+    from foo_bar_baz import foo_bar_baz
     assert foo_bar_baz(0) == ""
     assert foo_bar_baz(1) == "1" 
 
